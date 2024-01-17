b1bc35557b813a53d75194d70883928a02124a8d
Yevheniia Barchuk <slobodenuyk9897@gmail.com>
2024-01-17 13:03:58 +0100

    addPoints

--- task-2.js
+++ task-2.js
@@ -3,6 +3,14 @@ function formatMessage(message, maxLength) {
         return message;
     }else if (message.length > maxLength) {
         const modifiMessage = message.slice(0, maxLength);
-        console.log(modifiMessage);
+       const addPointsInMessage = modifiMessage.padEnd(3, '...');
+       return addPointsInMessage;
     }
 }
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); 
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
